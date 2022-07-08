718a0a702f5f4392b772726b22c055bd252c17c5
imangolf1313 <osvaldo.lopez.is@unipolidgo.edu.mx>
2022-07-08 08:30:59 -0500

    Notificaciones
    uno de expo notifications

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import UserStack from './UserStack';
-import InicioSesion from './InicioSesion';
-import Subir from './Subir';
-import Login from './Login';
+import React, { useState, useEffect, useRef } from "react";
+import { Text, View, Platform } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import UserStack from "./UserStack";
+import InicioSesion from "./InicioSesion";
+import Subir from "./Subir";
+import Login from "./Login";
+import Notificaciones from "./Notificaciones";
+import { getAuth } from "firebase/auth";
 
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Home!</Text>
     </View>
   );
@@ -17,7 +19,7 @@ function HomeScreen() {
 
 function SettingsScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Settings!</Text>
     </View>
   );
@@ -26,15 +28,16 @@ function SettingsScreen() {
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const auth = getAuth();
   return (
     <NavigationContainer>
       <Tab.Navigator>
         {/* <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} /> */}
         {/* <Tab.Screen name="Users" component={UserStack}  options={{ headerShown: false }} /> */}
-        <Tab.Screen name="Login" component={Login} />
-        <Tab.Screen name="Registro" component={InicioSesion}  />
-        <Tab.Screen name="Imagenes" component={Subir}  />
+        <Tab.Screen name="Notificaciones" component={Notificaciones} />
+        <Tab.Screen name="Registro" component={InicioSesion} />
+        <Tab.Screen name="Imagenes" component={Subir} />
       </Tab.Navigator>
     </NavigationContainer>
   );
